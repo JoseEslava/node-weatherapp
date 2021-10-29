893e63583b165081550db3b320dd063b763826fa
Jose Eslava <jeslava@hotmail.com>
2021-10-28 22:16:56 -0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlerbar engine and views
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        titulo: 'Weather App',
+        nombre: 'Jose Eslava'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        titulo: 'Solicita tu ticket de servicio',
+        nombre: 'Jose Eslava'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        titulo: 'Conocenos!',
+        nombre: 'Jose Eslava'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'No se encontró la dirección'
+        })
+    }
+
+    forecast(req.query.address, (error, forecastData) => {
+        if (error) {
+            res.send({ error })
+        }
+
+        res.send({
+            clima: forecastData,
+            address: req.query.address
+        })
+    
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        titulo: '404 Page',
+        nombre: 'Jose Eslava',
+        errorMessage: 'Help article does not exist'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        titulo: '404 Page',
+        nombre: 'Jose Eslava',
+        errorMessage: 'Page does not exist'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on server 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (city, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5fd5a04855a35b4d163b3aaf9da136d4&query=' + city
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location!', undefined)
+        }     
+        else {
+            callback(undefined, response.body.current.weather_descriptions[0] + '. Temperatura actual: ' + response.body.current.temperature + ' grados. Temperatura en el exterior: ' + response.body.current.feelslike + ' grados')
+        }
+    })
+}
+
+module.exports = forecast
